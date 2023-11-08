d1bf13643d7d51451bd1f839275c62970a5acc3b
Fahreiza <fahreizataura@gmail.com>
2023-11-08 12:04:50 +0700

    rata rata

---
+++ Jobsheet9/ArrayRataNilai09.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ArrayRataNilai11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMahasiswa = sc.nextInt();
+        int[] nilaiMhs = new int[jumlahMahasiswa];
+        double total = 0;
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilaiMhs[i] = sc.nextInt();
+            total += nilaiMhs[i];
+        }
+        double rata2 = total / jumlahMahasiswa;
+        System.out.println("Rata-rata nilai = " + rata2);
+    }
+}
