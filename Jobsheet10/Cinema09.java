6ee71cc86c84a3c5cc5811c914c714aea8dd8ff1
Fahreiza <fahreizataura@gmail.com>
2023-11-10 15:25:38 +0700

    inisialisasi

--- Jobsheet10/Cinema09.java
+++ Jobsheet10/Cinema09.java
@@ -2,16 +2,12 @@ package Jobsheet10;
 
 public class Cinema09 {
     public static void main(String[] args) {
-        String[][] audience = new String[4][2];
-        
-        audience[0][0] = "Amin";
-        audience[0][1] = "Bena";
-        audience[1][0] = "Candra";
-        audience[1][1] = "Dela";
-        audience[2][0] = "Eka";
-        audience[2][1] = "Farhan";
-        audience[3][0] = "Gisel";
-        audience[3][1] = "Hana";
+        String[][] audience = {
+            {"Amin", "Bena"},
+            {"Candra", "Dela"},
+            {"Eka", "Farhan"},
+            {"Gisel", "Hana"}
+        };
         
         for (int i = 0; i < audience.length; i++) {
             System.out.println("Contents of row " + (i + 1) + ": " + String.join(", ", audience[i]));
@@ -27,7 +23,7 @@ public class Cinema09 {
         for (String [] rowAudience : audience) {
             System.out.println("lenght of row" + rowAudience.length);
         }
-        
+
         System.out.println("\nAudience in the row 3 : ");
             for (String i : audience[2]){
             System.out.println(i);
