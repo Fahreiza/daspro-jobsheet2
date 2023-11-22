b778074aa2387986d3413a7230c92acce21836d1
Fahreiza <fahreizataura@gmail.com>
2023-11-22 13:56:47 +0700

    commit

--- Jobsheet7/Formutiple09.java
+++ Jobsheet7/Formutiple09.java
@@ -1,6 +1,7 @@
 package Jobsheet7;
 
 import java.util.Scanner;
+
 public class Formutiple09 {
     public static void main(String[] args) {
         Scanner input09 = new Scanner(System.in);
@@ -20,6 +21,7 @@ public class Formutiple09 {
                 average = sum / counter;
             }
         }
+
         System.out.printf("There are d numbers that are multiple of %d in range 1 to 50.\n", counter, multiple);
         System.out.printf("The sum from all multiples of %d in range 1 s.d. 50 is %d. \n", multiple, sum);
         System.out.printf("The average is %d", average);
