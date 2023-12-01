f4b03cda5f27169c38ee8515db6d9b9b687405dd
Fahreiza <fahreizataura@gmail.com>
2023-12-01 20:23:07 +0700

    Jobsheet 14

---
+++ Jobsheet14/PowerRecursive21.java
@@ -0,0 +1,35 @@
+package Jobsheet14;
+
+import java.util.Scanner;
+
+public class PowerRecursive21 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input Base Number: ");
+        int base = input.nextInt();
+        System.out.print("Input Power Number: ");
+        int Power = input.nextInt();
+        System.out.println("Result of " + base + " power " + Power + " = " + calculatePower(base, Power));
+
+        printPower(base, Power);
+    }
+
+    static int calculatePower(int base, int pow) {
+        if (pow == 0)
+            return 1;
+        else
+            return base * calculatePower(base, pow - 1);
+    }
+
+    static void printPower(int base, int pow) {
+        String series = "";
+        for (int i = 0; i < pow; i++) {
+            series += base;
+            if (i < pow - 1) {
+                series += "x";
+            }
+        }
+        series += "x1";
+        System.out.println(series + " = " + calculatePower(base, pow));
+    }
+}
---
+++ Jobsheet14/ProvitRecursive09.java
@@ -0,0 +1,22 @@
+package Jobsheet14;
+import java.util.Scanner;
+
+public class ProvitRecursive09 {
+    private static String base;
+
+    static double calculateProfit(double Balance, int Period) {
+        if (Period == 0)
+            return Balance;
+        else
+            return 1.11 * calculateProfit(Balance, Period - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input Balance: ");
+        double initialBalance = input.nextInt();
+        System.out.print("Input Investment Period: ");
+        int investPeriod = input.nextInt();
+        System.out.println("Balance after  " + base + " year = " + calculateProfit(initialBalance, investPeriod));
+    }
+}
